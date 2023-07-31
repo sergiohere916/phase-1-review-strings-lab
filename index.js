bad18916d05da55673cbb0c7d8a82d5e4a8dcecd
Sergio Heredia <sergioheredia1996@gmail.com>
2023-07-31 10:35:09 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+let currentUser = "Sergio Heredia";
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let shortGreeting = "Welcome, " + currentUser.slice(0,1) + "!";
+
+
+
